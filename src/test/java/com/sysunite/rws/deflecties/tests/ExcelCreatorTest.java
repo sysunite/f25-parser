2f819696ef0496e41570ecba3fedc5bd60bfb4fd
Mohamad Alamili <mohamad@sysunite.com>
2016-05-01 21:21:45 +0200

    Test classes formatting

--- src/test/java/com/sysunite/rws/deflecties/tests/ExcelCreatorTest.java
+++ src/test/java/com/sysunite/rws/deflecties/tests/ExcelCreatorTest.java
@@ -1,24 +1,26 @@
 package com.sysunite.rws.deflecties.tests;
 
-import static org.junit.Assert.*;
+import com.sysunite.rws.deflecties.ExcelCreator;
+import org.junit.Test;
 
 import java.util.List;
 
-import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
-import com.sysunite.rws.deflecties.ExcelCreator;
-
+/**
+ * @author Mohamad Alamili <mohamad@sysunite.com>
+ */
 public class ExcelCreatorTest {
-	// change this to the dir where you have a lot of data files
-	private static final String DATA_DIR = "../data/f25";
-		
-	@Test
-	public void test() {
-		ExcelCreator ec = new ExcelCreator();
-		List<String> errors = ec.convertF25Files(DATA_DIR, "../output/out.xls");
-		for (String e: errors) System.out.println(e);
-		// this assertion may fail if you have bad sample files, but the output
-		// file may still be written
-		assertTrue(errors.isEmpty());
-	}
+  // change this to the dir where you have a lot of data files
+  private static final String DATA_DIR = "../data/f25";
+
+  @Test
+  public void test() {
+    ExcelCreator ec = new ExcelCreator();
+    List<String> errors = ec.convertF25Files(DATA_DIR, "../output/out.xls");
+    for (String e : errors) System.out.println(e);
+    // this assertion may fail if you have bad sample files, but the output
+    // file may still be written
+    assertTrue(errors.isEmpty());
+  }
 }
--- src/test/java/com/sysunite/rws/deflecties/tests/LargeScaleTest.java
+++ src/test/java/com/sysunite/rws/deflecties/tests/LargeScaleTest.java
@@ -1,47 +1,47 @@
 package com.sysunite.rws.deflecties.tests;
 
-import java.io.File;
-
+import com.sysunite.rws.deflecties.F25File;
 import org.junit.Test;
 
-import com.sysunite.rws.deflecties.F25File;
+import java.io.File;
 
+/**
+ * @author Mohamad Alamili <mohamad@sysunite.com>
+ */
 public class LargeScaleTest {
-	// change this to the dir where you have a lot of data files
-	private static final String DATA_DIR = "../data/f25/";
-		
-	/*
-	 * Recursively parses each f25 file in a directory, writes errors and
-	 * number of warnings.
-	 */
-	@Test
-	public void test() {
-		File f = new File(DATA_DIR);
-		recurse(f);
-	}
+  // change this to the dir where you have a lot of data files
+  private static final String DATA_DIR = "../data/f25/";
 
-	private void recurse(File f) {
-		if (f.isDirectory()) {
-			for (File f2: f.listFiles()) {
-				recurse(f2);
-			}
-		}
-		else {
-			if (f.getName().toLowerCase().endsWith(".f25")) {
-				System.out.println("-------\n" + f.getAbsolutePath());
-				try {
-					F25File f25 = new F25File(f);
-					if (!f25.errors.isEmpty()) {
-						System.out.println("Errors:");
-						for (String err: f25.errors) System.out.println("  " + err);
-					}
-					System.out.println("Warnings: " + f25.warnings.size());
-				}
-				catch (Exception e) {
-					System.out.println("Exception: " + e.getMessage());
-				}
-			}
-		}
-	}
+  /*
+   * Recursively parses each f25 file in a directory, writes errors and
+   * number of warnings.
+   */
+  @Test
+  public void test() {
+    File f = new File(DATA_DIR);
+    recurse(f);
+  }
+
+  private void recurse(File f) {
+    if (f.isDirectory()) {
+      for (File f2 : f.listFiles()) {
+        recurse(f2);
+      }
+    } else {
+      if (f.getName().toLowerCase().endsWith(".f25")) {
+        System.out.println("-------\n" + f.getAbsolutePath());
+        try {
+          F25File f25 = new F25File(f);
+          if (!f25.errors.isEmpty()) {
+            System.out.println("Errors:");
+            for (String err : f25.errors) System.out.println("  " + err);
+          }
+          System.out.println("Warnings: " + f25.warnings.size());
+        } catch (Exception e) {
+          System.out.println("Exception: " + e.getMessage());
+        }
+      }
+    }
+  }
 
 }
